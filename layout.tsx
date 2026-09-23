17fca55c5ceadef4a89bf59037e77d7be8a2ef3b
Marriel Baet <118659526+mbbaet@users.noreply.github.com>
2026-09-23 20:38:38 +0800

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Laguna College ICT Services",
+  description: "MS Teams Support Portal",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
